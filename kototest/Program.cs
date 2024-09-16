0b3daafbf7193fd011cb44ad46f83ed11d7fc4fb
Timofey.Lobachev <timotey2005@gmail.com>
2024-09-16 17:17:40 +0500

    Кототестовая программа

---
+++ kototest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace kototest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое слагаемое");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите second слагаемое");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Summa chisel равна" + (x+y));
+
+            Console.ReadKey();
+        }
+    }
+}
